58a9a5570b9f24fb19e8c6d29d5f913fdb2ed7d8
Sandrine Poissonnet <spoisson@gmail.com>
2021-10-05 18:22:21 -0700

    sideDrawer set up

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import {
 	Redirect,
 	Switch,
 } from 'react-router-dom';
+import MainNavigation from './shared/Navigation/MainNavigation';
 import Users from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
 	return (
 		<Router>
-			<Switch>
-				<Route exact path="/">
-					<Users />
-				</Route>
-				<Route exact path="/places/new">
-					<NewPlace />
-				</Route>
-				<Redirect to="/" />
-			</Switch>
+			<MainNavigation />
+			<main>
+				<Switch>
+					<Route exact path="/">
+						<Users />
+					</Route>
+					<Route exact path="/places/new">
+						<NewPlace />
+					</Route>
+					<Redirect to="/" />
+				</Switch>
+			</main>
 		</Router>
 	);
 };
